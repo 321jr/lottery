3c0d02357461be884e817060df92cc5fa5074d88
321jr <51755783+321jr@users.noreply.github.com>
2021-09-02 12:47:09 +0800

    Update js/member.js

--- js/member.js
+++ js/member.js
@@ -1,7 +1,7 @@
 var member = [
   {
     "phone": "No.2038",
-    "name": "于洁"
+    "name": "张和贵"
   },
   {
     "phone": "No.0282",
@@ -9,82 +9,58 @@ var member = [
   },
   {
     "phone": "No.3392",
-    "name": "王巍"
+    "name": "张勇"
   },
   {
     "phone": "No.8080",
-    "name": "曾云翔"
+    "name": "郭静"
   },
   {
     "phone": "No.3855",
-    "name": "张莉莎"
+    "name": "雷月华"
   },
   {
     "phone": "No.6389",
-    "name": "李丽娜"
+    "name": "刘建琳"
   },
   {
     "phone": "No.4440",
-    "name": "韩愈"
+    "name": "李万知"
   },
   {
     "phone": "No.3170",
-    "name": "余梦迪"
+    "name": "吕小莉"
   },
   {
     "phone": "No.1688",
-    "name": "郭浪屿"
+    "name": "沈兴容"
   },
   {
     "phone": "No.0018",
-    "name": "王慧琳"
+    "name": "田兴明"
   },
   {
     "phone": "No.0318",
-    "name": "黄务莎"
+    "name": "王凤琴"
   },
   {
     "phone": "No.7712",
-    "name": "陈龙"
+    "name": "曾丽娟"
   },
   {
     "phone": "No.6561",
-    "name": "张笑丝"
+    "name": "张震"
   },
   {
     "phone": "No.1260",
-    "name": "李晓璐"
+    "name": "张劲"
   },
   {
     "phone": "No.6280",
-    "name": "汪钇宏"
+    "name": "钟兴强"
   },
   {
     "phone": "No.8517",
-    "name": "罗川洪"
-  },
-  {
-    "phone": "No.2335",
-    "name": "李昊阳"
-  },
-  {
-    "phone": "No.2681",
-    "name": "陈沁"
-  },
-  {
-    "phone": "No.4024",
-    "name": "徐浪"
-  },
-  {
-    "phone": "No.0463",
-    "name": "李墨"
-  },
-  {
-    "phone": "No.2423",
-    "name": "王彦博"
-  },
-  {
-    "phone": "No.5357",
-    "name": "甘辛"
+    "name": "曹紫薇"
   }
 ]
